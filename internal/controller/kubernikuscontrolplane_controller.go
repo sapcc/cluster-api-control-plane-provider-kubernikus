5dd4e2434f25aa2561ec192cc45ba0332f0eafab
Dmitri Fedotov <dmitri.fedotov@sap.com>
2024-02-28 18:29:37 +0200

    fix rbac to update secrets

--- internal/controller/kubernikuscontrolplane_controller.go
+++ internal/controller/kubernikuscontrolplane_controller.go
@@ -48,7 +48,7 @@ type KubernikusControlPlaneReconciler struct {
 //+kubebuilder:rbac:groups=controlplane.cluster.x-k8s.io,resources=kubernikuscontrolplanes/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=controlplane.cluster.x-k8s.io,resources=kubernikuscontrolplanes/finalizers,verbs=update
 //+kubebuilder:rbac:groups=cluster.x-k8s.io,resources=clusters,verbs=get;list;watch;update;patch
-//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create
+//+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
